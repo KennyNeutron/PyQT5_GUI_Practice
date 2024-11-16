be4427f331be4e329b044ef0f65eec79d03f2e69
Kenny Neutron <89734947+KennyNeutron@users.noreply.github.com>
2024-11-16 17:30:21 +0800

    UPDATE: modified the simple buttons and event to use Class (programming)

--- Buttons&Events.py
+++ Buttons&Events.py
@@ -2,24 +2,30 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow #QMainWindow is similar to QWidget(s)
 import sys
 
-def clicked_button1():
-    print("Button 1 clicked")
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(0, 0, 500, 500)  #xpos, ypos, width, height (xpos and ypos are coordinates on your screen)
+        self.setWindowTitle("My First App")
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Hello World")
+        self.label.move(100, 100)
+
+        self.btn_Button1 = QtWidgets.QPushButton(self)
+        self.btn_Button1.setText("Button 1")
+        self.btn_Button1.move(200, 200)
+        self.btn_Button1.clicked.connect(self.clicked_button1)
     
+    def clicked_button1(self):
+        self.label.setText("Button 1 clicked 01234567890")
+
 
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(0, 0, 500, 500)  #xpos, ypos, width, height (xpos and ypos are coordinates on your screen)
-    win.setWindowTitle("My First App")
-
-    label = QtWidgets.QLabel(win)
-    label.setText("Hello World")
-    label.move(100, 100)
-
-    btn_Button1 = QtWidgets.QPushButton(win)
-    btn_Button1.setText("Button 1")
-    btn_Button1.move(200, 200)
-    btn_Button1.clicked.connect(clicked_button1)
+    win = MyWindow()
 
     win.show()
     sys.exit(app.exec_())
