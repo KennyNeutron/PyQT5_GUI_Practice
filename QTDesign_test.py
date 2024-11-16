584b1909c462b2ba24ca9081ac28809df26754f9
Kenny Neutron <89734947+KennyNeutron@users.noreply.github.com>
2024-11-16 23:13:26 +0800

    UPDATE: addition now works, bugs on float nums

--- QTDesign_test.py
+++ QTDesign_test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Test_QTdesigner.ui'
+# Form implementation generated from reading ui file 'Test_QTDesigner.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
@@ -15,54 +15,53 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1116, 932)
-
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
         self.btn_ClickMe = QtWidgets.QPushButton(self.centralwidget)
         self.btn_ClickMe.setGeometry(QtCore.QRect(470, 390, 231, 141))
         self.btn_ClickMe.setObjectName("btn_ClickMe")
+        self.btn_ClickMe.clicked.connect(self.button_clicked)
 
         self.lbl_MainLabel = QtWidgets.QLabel(self.centralwidget)
         self.lbl_MainLabel.setGeometry(QtCore.QRect(430, 270, 341, 61))
-
         font = QtGui.QFont()
         font.setPointSize(24)
-
         self.lbl_MainLabel.setFont(font)
         self.lbl_MainLabel.setObjectName("lbl_MainLabel")
 
         MainWindow.setCentralWidget(self.centralwidget)
-
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1116, 21))
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
-
         MainWindow.setMenuBar(self.menubar)
-
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
-
         MainWindow.setStatusBar(self.statusbar)
-
         self.menubar.addAction(self.menuFile.menuAction())
 
         self.retranslateUi(MainWindow)
-        
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.btn_ClickMe.setText(_translate("MainWindow", "Click Me!"))
-        self.lbl_MainLabel.setText(_translate("MainWindow", "Hello this is Qt Designer"))
+        self.lbl_MainLabel.setText(
+            _translate("MainWindow", "Hello this is Qt Designer")
+        )
         self.menuFile.setTitle(_translate("MainWindow", "File"))
-
+    
+    def button_clicked(self):
+        self.lbl_MainLabel.setText("Button Clicked")
+        print("Button Clicked")
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
