41d87d808b0eea3e6192a9f1e03ed8ed59f6c52f
Kenny Neutron <89734947+KennyNeutron@users.noreply.github.com>
2024-11-16 20:18:07 +0800

    UPDATE: added simple calculator gui that was designed using qt designer

--- QTDesigner_test.py
+++ QTDesigner_test.py
@@ -15,30 +15,42 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1116, 932)
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.btn_ClickMe = QtWidgets.QPushButton(self.centralwidget)
         self.btn_ClickMe.setGeometry(QtCore.QRect(470, 390, 231, 141))
         self.btn_ClickMe.setObjectName("btn_ClickMe")
+
         self.lbl_MainLabel = QtWidgets.QLabel(self.centralwidget)
         self.lbl_MainLabel.setGeometry(QtCore.QRect(430, 270, 341, 61))
+
         font = QtGui.QFont()
         font.setPointSize(24)
+
         self.lbl_MainLabel.setFont(font)
         self.lbl_MainLabel.setObjectName("lbl_MainLabel")
+
         MainWindow.setCentralWidget(self.centralwidget)
+
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1116, 21))
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
+
         MainWindow.setMenuBar(self.menubar)
+
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
+
         MainWindow.setStatusBar(self.statusbar)
+
         self.menubar.addAction(self.menuFile.menuAction())
 
         self.retranslateUi(MainWindow)
+        
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
--- app.py
+++ app.py
@@ -8,6 +8,10 @@ def window():
     win.setGeometry(0, 0, 500, 500)  #xpos, ypos, width, height (xpos and ypos are coordinates on your screen)
     win.setWindowTitle("My First App")
 
+    label = QtWidgets.QLabel(win)
+    label.setText("Hello World")
+    label.move(100, 100)
+
     win.show()
     sys.exit(app.exec_())
 
